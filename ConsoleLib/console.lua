0eb47b978277e07d8bf80268365a16e4190f4766
AgentEnder <agentendercake@gmail.com>
2017-05-24 23:41:24 -0400

    Addd some comments to console.lua
    removed game-specific code and added some comments

--- ConsoleLib/console.lua
+++ ConsoleLib/console.lua
@@ -32,28 +32,10 @@ end
 function checkStr(str)
 	if devConsole == false then
 		local fail = false
-		if str == "single player" then
-			singlePlayerMenu.load()
-		elseif str == "death" then
-			death.load()
-		elseif str == "player2.isai = false" then
-			player2.isAi = false
-		elseif str == "player2.isai = true" then
-			player2.isAi = true
-		elseif str == "heal" then
-			player1.health = player1.maxhealth
-		elseif str == "extra dmg" then
-			player1.dmg = 50
-		elseif str == "player = knight" then
-			class.set(player1, knight)
-		elseif str == "map3" then
-			playing.load(map3)
-		elseif str == "config" then
-			options.load()
-		elseif str =="reset highscores" then
-				love.filesystem.write("sav.lua", "highscore.easy = 0 highscore.normal = 0 highscore.hard = 0")
-		elseif str == "devConsole = true" then
-			devConsole = true
+		if str == "settingNum1" then //Check for various phrases (cheat-codes)
+			//Do something based on cheat code entered
+		elseif str == "devConsole" //Enable developer console
+			devConsole = true //Turn on developer console
 		else
 			fail = true
 		end
@@ -63,11 +45,11 @@ function checkStr(str)
 			playSound(consoleSound)
 		end
 		consoleStr = ""
-	else
-		print("Running Code:" .. consoleStr)
-		love.filesystem.write("dev.lua", consoleStr)
-		local chunk = love.filesystem.load("dev.lua", consoleStr)
-		chunk()
-		consoleStr = ""
+	else //Use dev console
+		print("Running Code:" .. consoleStr) //Tell user what they are running
+		love.filesystem.write("dev.lua", consoleStr) //save code to file
+		local chunk = love.filesystem.load("dev.lua", consoleStr) //load file
+		chunk() //Run file
+		consoleStr = "" //reset consoleStr
 	end
 end
